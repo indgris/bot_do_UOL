8f04647c63a2142ed6e35a52ffe87d2360d2aec2
Ingrid Fernandes <114759158+indgris@users.noreply.github.com>
2023-03-20 19:21:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
   return "Olá, mundo!"
+
+@app.route("/sobre")
+def sobre():
+  return "Sobre"
+
+@app.route("/contato")
+def contato():
+  return "contato@algum.com"
