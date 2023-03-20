78811c573bfd9f5a48a9175bbd5d4b0c99d76c50
Ingrid Fernandes <114759158+indgris@users.noreply.github.com>
2023-03-20 20:41:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from tchan import ChannelScraper
 
 app = Flask(__name__)
 
 menu = """
 
-<a href="/">Página inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a> |  <a href="/portfolio">Portfólio</a>
+<a href="/">Página inicial</a> | <a href="/sobre">Sobre</a> | <a href="/contato">Contato</a> |  <a href="/portfolio">Portfólio</a> | <a href="/promocoes">Promoções</a>
 <br>
 """
 
@@ -23,3 +24,20 @@ def contato():
 @app.route("/portfolio")
 def portfolio():
   return menu + "https://www.behance.net/indgri"
+
+@app.route("/promocoes")
+def promocoes():
+  conteudo = menu + """
+  Encontrei as seguintes promoções no <a href="https://t.me/promocoeseachadinhos">@promocoeseachadinhos</a>:
+  <br>
+  <ul>
+  """
+  scraper = ChannelScraper()
+  contador = 0
+  for message in scraper.messages("promocoeseachadinhos"):
+    contador += 1
+    texto = message.text.strip().splitlines()[0]
+    conteudo += f"<li>{message.created_at} {texto}</li>"
+    if contador == 10:
+      break
+  return conteudo + "</ul>"
